cec95954c6a971a4230b844219efc3f90b2e06fc
kebi <kanglu-email@qq.com>
2026-05-20 14:51:58 +0800

    feat: 更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react'
+
 export default function App() {
-  return (
-    <main className="app-shell">
-      <h1>React in Turbo Core Template</h1>
-      <p>This app was created inside the current monorepo architecture.</p>
-    </main>
-  )
+    const [count, setCount] = useState(new Date().toTimeString())
+
+    setInterval(() => {
+        setCount(() => new Date().toTimeString())
+    }, 1000)
+
+    return (
+        <main className='app-shell'>
+            <h1>React in Turbo Core Template1</h1>
+            <p>
+                This app was created inside the current monorepo architecture.
+            </p>
+            <p>{count}</p>
+        </main>
+    )
 }
