06a555f0a4cd9c101ba4eab2d681bb9917ef9c7a
kebi <kanglu-email@qq.com>
2026-05-20 17:42:25 +0800

    feat: 新增uno.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
 import { RepoErrorBoundary } from '@repo/react-components'
+
+import 'uno.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
