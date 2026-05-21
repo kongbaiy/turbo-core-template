9764e01580de13966cf80bffd2af2ede128acf13
kebi <kanglu-email@qq.com>
2026-05-21 11:41:56 +0800

    feat(eslint.config.ts): 注入自动导入组件类型

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,7 +7,13 @@ export default reactConfig([
         languageOptions: {
             globals: {
                 ...eslintrcAutoImport.globals,
+                // 如果有需要额外声明的全局变量，也可以加在这里
+                // React: 'readonly',
             },
+        },
+        rules: {
+            // 确保 JSX 中未定义的组件会报错（但 globals 已声明，不会误报）
+            'react/jsx-no-undef': 'error',
         },
     },
 ])
