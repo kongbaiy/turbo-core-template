312ce834324b708386b8c09dbbebbe2834b66c0d
kebi <kanglu-email@qq.com>
2026-05-21 10:07:21 +0800

    feat: 新增自动导入配置

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,13 @@
 import { reactConfig } from '@repo/eslint-config'
+import eslintrcAutoImport from './.eslintrc-auto-import.json'
 
-export default reactConfig
+export default reactConfig([
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            globals: {
+                ...eslintrcAutoImport.globals,
+            },
+        },
+    },
+])
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { RepoErrorBoundary } from '@repo/react-components'
-
 import 'uno.css'
 import './index.css'
 
